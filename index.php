8c2176615549e3159a7ba582b828d060075afaac
KirillGul <50946729+KirillGul@users.noreply.github.com>
2021-12-20 22:05:46 +0300

    Начало 1

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-require_once 'application/bootstrap.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
+mb_internal_encoding('UTF-8');
+
+//require_once 'application/bootstrap.php';
 
 echo $_SERVER['REQUEST_URI'];
+$routes1 = explode('/', $_SERVER['REQUEST_URI']);
+var_dump($routes1);
